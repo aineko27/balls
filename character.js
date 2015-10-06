8861ef42cbb50cbee1ab2d04d7297f3fd33e6244
ganeko <zawawabis@gmail.com>
2015-10-06 18:56:03 +0900

    init

---
+++ character.js
@@ -0,0 +1,172 @@
+//物体を定義する関数---------------------------------------------------------------------------------------------------------------
+
+
+var Character = function(){
+	//諸々の設定
+	this.position = new Point();
+	this.velocity = new Point();
+	this.alive = false;
+	this.color = 0;
+	this.size = 0;
+	this.weight = 0;
+	this.absorption = false;
+}
+
+Character.prototype.set = function(p, s, v, c){
+	//座標、速度、サイズをセット
+	this.position.x = p.x;
+	this.position.y = p.y;
+
+	this.velocity.x = v.x;
+	this.velocity.y = v.y;
+
+	this.color = c;
+	this.size = s;
+	this.weight = s * s * s;
+	//生存フラグを立てる
+	this.alive = true;
+};
+
+
+
+//クリック時に動作する関数
+Character.prototype.strokeDottedLine = function(p){
+	var d = this.position.distance(p).length();
+	var rad = Math.atan2( (p.y - this.position.y), (p.x - this.position.x) )
+	var space = 10;
+	var dotted = Math.round( (d - this.size) / space / 2 );
+	
+	var p1x, p1y, p2x, p2y;
+
+	ctx.beginPath();
+console.log(dotted)
+	for(var i = 0; i < dotted - 0 ; i++){
+
+		p1x = this.position.x + (this.size + space * 2 * i) * Math.cos(rad);
+		p1y = this.position.y + (this.size + space * 2 * i) * Math.sin(rad);
+		p2x = this.position.x + (this.size + space * (2 * i + 1)) * Math.cos(rad);
+		p2y = this.position.y + (this.size + space * (2 * i + 1)) * Math.sin(rad);
+
+		ctx.moveTo(p1x, p1y);
+		ctx.lineTo(p2x, p2y);
+		ctx.closePath();
+	}
+
+
+	ctx.lineWidth = 3;
+	ctx.lineCap = "round";
+	ctx.strokeStyle = DOTTED_LINE_COLOR;
+	ctx.stroke();
+};
+
+
+//物体の動きを制御する関数---------------------------------------------------------------------------------------------------------
+
+
+//自由落下
+Character.prototype.fall = function(){
+	this.velocity.y -= 0.3;
+};
+
+
+//速度を位置情報に変換
+Character.prototype.move = function(){
+	//速度の上限を設定
+	if(this.velocity.x >=  10) this.velocity.x =  10
+	if(this.velocity.x <= -10) this.velocity.x = -10
+
+	//速度を位置情報に変換
+	this.position.x += this.velocity.x;
+	this.position.y -= this.velocity.y;
+
+	//位置情報の上限を設定
+	if(this.position.y >= 243 - this.size) this.position.y = 243 - this.size;
+	if(this.position.x <= this.size ) this.position.x = this.size;
+	if(this.position.x >= 256 - this.size) this.position.x = 256 - this.size;
+};
+
+
+//物体間の衝突においてめり込んだ分の補正を行う
+Character.prototype.positionCorrect = function(p){
+	//物体二点間の差を表すベクトル、距離、めり込んだ長さをを求める
+	var vx = p.position.x - this.position.x;
+	var vy = p.position.y - this.position.y;
+	var len = Math.sqrt(vx * vx + vy * vy);
+	var excess = (this.size + p.size) - len;
+
+	//ベクトルの正規化を行う、長さが0以下の場合はエラーを打ち出してゲームを止める
+	if(len > 0){
+		len = 1 / len;
+		vx *= len;
+		vy *= len;
+	}
+	else console.log("Character.positionCorrection ERROR");
+
+	//めり込んだ分を押し出すように補正する
+	excess /= this.size + p.size;
+	p.position.x += vx * excess * this.size;
+	p.position.y += vy * excess * this.size;
+	this.position.x -= vx * excess * p.size;
+	this.position.y -= vy * excess * p.size;
+};
+
+
+//物体間の衝突後の速度をそれぞれ求める
+Character.prototype.collisionCalculate = function(p){
+	//それぞれの速度を重心方向(mx,my)と接戦方向(rx,ry)に分解する
+	var t;
+	var vx =  (p.position.x - this.position.x);
+	var vy = -(p.position.y - this.position.y);
+
+	t = - ( vx * this.velocity.x + vy * this.velocity.y) / (vx * vx + vy * vy);
+	var arx = this.velocity.x + vx * t;
+	var ary = this.velocity.y + vy * t;
+
+	t = - (-vy * this.velocity.x + vx * this.velocity.y) / (vx * vx + vy * vy);
+	var amx = this.velocity.x - vy * t;
+	var amy = this.velocity.y + vx * t;
+
+	t = - ( vx * p.velocity.x + vy * p.velocity.y) / (vx * vx + vy * vy);
+	var brx = p.velocity.x + vx * t;
+	var bry = p.velocity.y + vy * t;
+
+	t = - (-vy * p.velocity.x + vx * p.velocity.y) / (vx * vx + vy * vy);
+	var bmx = p.velocity.x - vy * t;
+	var bmy = p.velocity.y + vx * t;
+
+	//反発係数の設定と重さの決定、衝突後の重心方向の値を求める
+	var e = 0.9;
+	var adx = (this.weight * amx + p.weight * bmx + bmx * e * p.weight - amx * e * p.weight) / (this.weight + p.weight);
+	var bdx = -e * (bmx - amx) + adx;
+	var ady = (this.weight * amx + p.weight * bmy + bmy * e * p.weight - amy * e * p.weight) / (this.weight + p.weight);
+	var bdy = -e * (bmy - amy) + ady;
+
+	//接戦方向速度と重心方向速度を足して衝突後の速度を求める
+	this.velocity.x = adx + arx;
+	this.velocity.y = ady + ary;
+	p.velocity.x = bdx + brx;
+	p.velocity.y = bdy + bry;
+};
+
+//二体の物体を結合する
+Character.prototype.absorptionCalculate = function(p){
+	//二体間の重心を求める
+	var cp = new Point();
+	cp.x = (this.weight * this.position.x + p.weight * p.position.x) / (this.weight + p.weight);
+	cp.y = (this.weight * this.position.y + p.weight * p.position.y) / (this.weight + p.weight);
+
+	//吸収後の速度を求める
+	var cv = new Point();
+	cv.x = (this.weight * this.velocity.x + p.weight * p.velocity.x) / (this.weight + p.weight);
+	cv.y = (this.weight * this.velocity.y + p.weight * p.velocity.y) / (this.weight + p.weight);
+
+	//古いほうのボールのabsorptionフラグを真にし、位置情報と速度、サイズ、質量を更新する
+	this.absorption = true;
+	p.position.x = cp.x;
+	p.position.y = cp.y;
+	p.velocity.x = cv.x;
+	p.velocity.y = cv.y;
+	p.size = Math.cbrt(p.weight + this.weight);
+	p.weight = p.size * p.size * p.size;
+
+}
---
+++ main.js
@@ -0,0 +1,282 @@
+// -global ------------------------------------------------------------------------------------------------------------------------
+
+var screenCanvas;
+var run = true;
+var fps = 1000 / 30;
+var mouse = new Point();
+var ctx;
+var counter;
+var creatF = false;
+var prepF = false;
+var fireF = false;
+var ck;
+
+
+
+
+// -const -------------------------------------------------------------------------------------------------------------------------
+
+var BALL_MAX_COUNT = 1024;
+var PLAYER_COLOR  　　= "rgba(  0, 255,   0, 0.85)";//緑
+var BALL_COLOR_01 　　= "rgba(  0,   0, 255, 0.60)";//青
+var BALL_COLOR_02　　 = "rgba(255,   0,   0, 0.60)";//赤
+var WALL_COLOR    　　= "rgba( 85,  85,  85, 0.75)";//グレー
+var DOTTED_LINE_COLOR = "rgba(255, 140,   0, 0.80)";//オレンジ
+
+
+
+
+// -main --------------------------------------------------------------------------------------------------------------------------
+
+//ページ読み込み時に起動するfunciton
+window.onload = function(){
+
+	//ローカル変数の定義
+	var i, j;
+	var p = new Point();
+	var v = new Point();
+
+
+	//スクリーンの初期化
+	screenCanvas = document.getElementById("screen");
+	screenCanvas.width = 256;
+	screenCanvas.height = 256;
+
+
+	//2dコンテキスト
+	ctx = screenCanvas.getContext("2d");
+
+
+	//イベントの登録
+	screenCanvas.addEventListener("mousemove", mouseMove, true);
+	screenCanvas.addEventListener("mousedown", mouseDown, true);
+	window.addEventListener("mouseup", mouseUp, true);
+	window.addEventListener("keydown", keyDown, true);
+
+
+	//球初期化
+	var ball= new Array(BALL_MAX_COUNT);
+	for(i = 0; i < BALL_MAX_COUNT; i++){
+		ball[i] = new Character;
+	}
+
+
+	//自機初期化
+	p.x = screenCanvas.width / 2;
+	p.y = screenCanvas.height / 2 -15;
+	v.x = 0;
+	v.y = 0;
+	ball[0].set(p, 10, v);
+
+	//レンダリング処理を呼び出す-----------------------------------------------------------------------------------------------
+
+	(function(){
+		//カcウンターの値を増やす
+		counter ++;
+
+
+
+
+
+		//入力による変更---------------------------------------------------------------------------------------------------
+
+		if(ck){
+			console.log("入力されたキーコードは " + ck)
+
+			if(ck === 67) creatF = true;
+
+		//キーコード初期化
+		ck = null;
+		}
+
+
+
+
+
+		//フラグ管理-------------------------------------------------------------------------------------------------------
+
+		//他機生成
+		if(creatF){
+			for(i = 0; i < BALL_MAX_COUNT; i++){
+				if(!ball[i].alive){
+					p.x = mouse.x;
+					p.y = mouse.y;
+					v.x = 0;
+					v.y = 0;
+					var s = Math.floor(Math.random() * 4) + 6;
+					var c = Math.floor(Math.random() * 2) + 1;
+					ball[i].set(p, s, v, c);
+					creatF = false;
+					break;
+				}
+			}
+		}
+
+
+
+		//球発射
+		if(fireF){
+			prepF = false;
+			fireF = false;
+		}
+
+
+
+		//物体の動きを制御-------------------------------------------------------------------------------------------------
+
+		//自由落下
+		for(i = 0; i < BALL_MAX_COUNT; i++){
+			if(ball[i].alive && !ball[i].absorption){
+				ball[i].fall();
+			}
+		}
+
+		//速度を位置情報に変換
+		for(i = 0; i < BALL_MAX_COUNT; i++){
+			if(ball[i].alive && !ball[i].absorption){
+				ball[i].move();
+			}
+		}
+
+		//地面との衝突
+		for(i = 0; i < BALL_MAX_COUNT; i++){
+			if(ball[i].alive && !ball[i].absorption){
+				if(ball[i].position.y >= 243 - ball[i].size){
+					//反発係数の設定とめり込んだ値を計算
+					var e = 0.6;
+					var excess = ball[i].position.y - (243 - ball[i].size);
+
+					ball[i].velocity.y += Math.sqrt(2 * 0.3 * e * excess) + 0.52 ;
+					ball[i].position.y = 243 - ball[i].size;
+					ball[i].velocity.y *= -e;
+				}
+			}
+		}
+
+		//壁との衝突
+		for(i = 0; i < BALL_MAX_COUNT; i++){
+			if(ball[i].alive && !ball[i].absorption){
+				if(ball[i].position.x <= ball[i].size){
+					ball[i].position.x = ball[i].size;
+					ball[i].velocity.x *= -0.9;
+				}
+				else if(ball[i].position.x >= 256 - ball[i].size){
+					ball[i].position.x = 256 - ball[i].size;
+					ball[i].velocity.x *= -0.9;
+				}
+			}
+		}
+
+		//ボール同士の衝突
+		for(i = 0; i < BALL_MAX_COUNT; i++){
+			for(j = i + 1; j < BALL_MAX_COUNT; j++){
+				if(ball[i].alive && !ball[i].absorption && ball[j].alive && !ball[j].absorption){
+					p = ball[j].position.distance(ball[i].position);
+					if( (p.length() < ball[j].size + ball[i].size) && (ball[i].color + ball[j].color === 3 || !i && ball[0].size < ball[j].size + 1) ){
+						//ボールのめり込んだ位置関係を元に戻す
+						ball[j].positionCorrect(ball[i]);
+						//速度ベクトルを重心方向と垂直な方向に分離し、衝突後の速度を求める
+						ball[j].collisionCalculate(ball[i]);
+					}
+					else if( p.length() < ball[j].size + ball[i].size - 2 && ball[i].color + ball[j].color !== 3){
+						if(!i && ball[0].size < ball[j].size + 1){
+							break;
+						}
+						//ボール同士を結合する
+						ball[j].absorptionCalculate(ball[i]);
+						
+					}
+				}
+			}
+		}
+
+
+
+
+
+		//画面の描画を行う-------------------------------------------------------------------------------------------------
+
+
+		//スクリーンクリア
+		ctx.clearRect(0, 0, screenCanvas.width, screenCanvas.height);
+
+		//背景の描画-------------------------------------------------
+
+		//地面の描画
+		ctx.beginPath();
+		ctx.fillStyle = WALL_COLOR;
+		ctx.fillRect(0, 243, screenCanvas.width, 10);
+
+		//動体の描画-------------------------------------------------
+
+		//自機の描画
+		ctx.beginPath();
+		ctx.arc(ball[0].position.x, ball[0].position.y, ball[0].size, 0, Math.PI * 2, true);
+		ctx.closePath();
+		ctx.fillStyle = PLAYER_COLOR;
+		ctx.fill();
+
+		//他機の描画
+
+		//青丸の描画
+		ctx.beginPath();
+		for(i = 1; i < BALL_MAX_COUNT; i++){
+			if(ball[i].alive && !ball[i].absorption && ball[i].color ===1){
+				ctx.arc(ball[i].position.x, ball[i].position.y, ball[i].size, 0, Math.PI * 2, true);
+				ctx.closePath();
+			}
+		}
+		ctx.fillStyle = BALL_COLOR_01;
+		ctx.fill();
+
+		//赤丸の描画
+		ctx.beginPath();
+		for(i = 1; i < BALL_MAX_COUNT; i++){
+			if(ball[i].alive && !ball[i].absorption && ball[i].color === 2){
+				ctx.arc(ball[i].position.x, ball[i].position.y, ball[i].size, 0, Math.PI * 2, true);
+				ctx.closePath();
+			}
+		}
+		ctx.fillStyle = BALL_COLOR_02;
+		ctx.fill();
+
+		//点線の描画
+		if(prepF){
+			ball[0].strokeDottedLine(mouse)
+			
+		}
+
+
+
+
+		//フラグにより再起呼び出し-----------------------------------------------------------------------------------------
+		if(run){setTimeout(arguments.callee, fps);}
+	})();
+};
+
+
+
+
+// -event--------------------------------------------------------------------------------------------------------------------------
+
+var mouseMove = function(event){
+	//マウスカーソルの座標の更新
+	mouse.x = event.clientX - screenCanvas.offsetLeft;
+	mouse.y = event.clientY - screenCanvas.offsetTop;
+}
+
+var keyDown = function(event){
+	//キーコードを取得
+	ck = event.keyCode;
+
+	//Escキーが押されていたらフラグを降ろす
+	if(ck === 27)run = false;
+}
+
+var mouseDown = function(event){
+	prepF = true;
+}
+
+var mouseUp = function(event){
+	fireF = true;
+}
